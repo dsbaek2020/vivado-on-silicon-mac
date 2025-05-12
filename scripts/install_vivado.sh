221f7bf4c97e6822d1d84efa02c6240c2c78e98a
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2025-05-12 17:40:52 +0900

    오프라인 설치 법으로 변경 2024.1 버전
    오프라인 설치 법으로 변경 2024.1 버전

--- scripts/install_vivado.sh
+++ scripts/install_vivado.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 
-# This runs the Vivado installer in batch mode.
+# This runs the Vivado offline installer in batch mode.
 
 script_dir=$(dirname -- "$(readlink -nf $0)";)
 source "$script_dir/header.sh"
 validate_linux
 
+# Read installation directory path from file
+install_dir_path=$(tr -d "\n\r\t " < "/home/user/scripts/install_dir")
 
-install_bin_path=$(tr -d "\n\r\t " < "/home/user/scripts/install_bin")
-
-file_hash=($(md5sum "$install_bin_path"))
-set_vivado_version_from_hash "$file_hash"
-
-# Extract installer
-f_echo "Extracting installer"
-eval "$install_bin_path --target /home/user/installer --noexec"
-
-# Get AuthToken by repeating the following command until it succeeds
-f_echo "Log into your Xilinx account to download the necessary files."
-while ! /home/user/installer/xsetup -b AuthTokenGen
-do
-	f_echo "Your account information seems to be wrong. Please try logging in again."
-	sleep 1
-done
-
-# Run installer
-f_echo "You successfully logged into your account. The installation will begin now."
-eula_args="XilinxEULA,3rdPartyEULA"
-
-# Check if the version is 202110 to include WebTalk terms
-if [ "$vivado_version" = "202110" ]; then
-    eula_args="${eula_args},WebTalkTerms"
-    f_echo "Note: The 2021.1 version enables WebTalk data collection and agrees automatically to the corresponding terms."
-    f_echo "For more information, see: https://docs.amd.com/r/2021.1-English/ug973-vivado-release-notes-install-license/WebTalk-Participation"
-    wait_for_user_input
-fi
-
-if /home/user/installer/xsetup -c "/home/user/scripts/install_configs/${vivado_version}.txt" -b Install -a "${eula_args}"
-then
-    f_echo "Vivado was successfully installed."
-    f_echo "Run start_container.sh to launch it."
-else
-    f_echo "An error occurred during installation. Please run cleanup.sh and try again."
+# Check if xsetup exists and is executable
+if [ ! -x "$install_dir_path/xsetup" ]; then
+    f_echo "Error: xsetup not found or not executable in $install_dir_path"
     exit 1
 fi
+
+f_echo "Found xsetup in $install_dir_path"
+
+# Set config file path for Vivado 2024.1
+config_file="/home/user/scripts/install_configs/202410.txt"
+
+# Verify config file exists
+if [ ! -f "$config_file" ]; then
+    f_echo "Error: Config file not found at $config_file"
+    exit 1
+fi
+
+f_echo "Using configuration file: $config_file"
+
+# Run xsetup in batch mode with EULA agreement
+f_echo "Starting Vivado 2024.1 offline installation..."
+
+if "$install_dir_path/xsetup" --agree XilinxEULA,3rdPartyEULA --batch Install --config "$config_file"; then
+    f_echo "Vivado was successfully installed."
+    f_echo "You can now run start_container.sh to launch Vivado."
+else
+    f_echo "An error occurred during installation. Please check the logs and run cleanup.sh before retrying."
+    exit 1
+fi
+
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,120 +4,108 @@
 
 script_dir=$(dirname -- "$(readlink -nf $0)";)
 source "$script_dir/header.sh"
+
 # Make sure that the script is run in macOS and not the Docker container
 validate_macos
 
 # Make sure permissions are right
-if [[ "$current_user" == "root" ]]
-then
-	f_echo "Do not execute this script as root."
-	exit 1
+if [[ "$current_user" == "root" ]]; then
+    f_echo "Do not execute this script as root."
+    exit 1
 fi
 
 # Make sure there are no previous installations in this folder
-if [ -d "$script_dir/../Xilinx" ]
-then
-	f_echo "A previous installation was found. To reinstall, remove the Xilinx folder."
-	exit 1
+if [ -d "$script_dir/../Xilinx" ]; then
+    f_echo "A previous installation was found. To reinstall, remove the Xilinx folder."
+    exit 1
 fi
 
 validate_internet
 
 f_echo "Advancing with the setup requires the following:"
-f_echo "- Agreeing to Xilinx'/AMD's EULAs (which can be obtained by extracting the installation binary)"
+f_echo "- Agreeing to Xilinx'/AMD's EULAs (which can be obtained by extracting the installation archive)"
 f_echo "- Enabling WebTalk data collection for version 2021.1 and agreeing to corresponding terms"
 f_echo "- Installation of Rosetta 2 and agreeing to Apple's corresponding software license agreement"
 f_echo "Proceed [Y/n]?"
 read user_consent
 case $user_consent in
 [yY]|[yY][eE]*)
-	f_echo "Continuing setup..."
-	;;
+    f_echo "Continuing setup..."
+    ;;
 [nN]|[nN][oO]*)
-	f_echo "Aborting setup."
-	exit 1
-	;;
+    f_echo "Aborting setup."
+    exit 1
+    ;;
 *)
-	f_echo "Invalid option."
-	exit 1
-	;;
+    f_echo "Invalid option."
+    exit 1
+    ;;
 esac
 
 # Check if the Mac is Intel or Apple Silicon
 if [[ "$(uname -m)" == "x86_64" ]]; then
-	f_echo "Mac is Intel-based. Rosetta installation is not required."
+    f_echo "Mac is Intel-based. Rosetta installation is not required."
 else
-	if arch -arch x86_64 uname -m > /dev/null 2>&1; then
-		f_echo "Rosetta is already installed."
-	else
-		f_echo "Rosetta is not installed."
-		f_echo "Proceeding with Rosetta installation..."
-		if ! softwareupdate --install-rosetta --agree-to-license; then
-			f_echo "Error installing Rosetta."
-			exit 1
-		fi
-	fi
+    if arch -arch x86_64 uname -m > /dev/null 2>&1; then
+        f_echo "Rosetta is already installed."
+    else
+        f_echo "Rosetta is not installed."
+        f_echo "Proceeding with Rosetta installation..."
+        if ! softwareupdate --install-rosetta --agree-to-license; then
+            f_echo "Error installing Rosetta."
+            exit 1
+        fi
+    fi
 fi
 
-# Get Vivado installation file
-f_echo "You need to put the Vivado installation file into this folder if you have not done so already."
-installation_binary=""
-while true
-do
-	installation_binary=""
-	# Get the absolute path to the file
-	f_echo "Then, drag and drop the Vivado installation binary into this terminal window and press Enter: "
-	read installation_binary
-	# check if it is accessible from the container
-	parent_dir=$(dirname "$script_dir")
-	if ! [[ $installation_binary == $parent_dir/* ]]
-	then
-		f_echo "You need to move the installation binary into the folder!"
-		continue
-	fi
-	# check file hash
-	file_hash=$(md5 -q "$installation_binary")
-	set_vivado_version_from_hash "$file_hash"
-	if [ "$?" -eq 0 ]
-	then
-		f_echo "Valid file provided. Detected version $vivado_version"
-		break
-	else
-		f_echo "File corrupted or version not supported."
-		continue
-	fi
+# Get Vivado installation directory
+f_echo "You need to put the extracted Vivado installation folder (containing xsetup) into this folder if you have not done so already."
+installation_dir=""
+while true; do
+    installation_dir=""
+    f_echo "Then, drag and drop the Vivado installation directory into this terminal window and press Enter: "
+    read installation_dir
+    parent_dir=$(dirname "$script_dir")
+
+    if ! [[ $installation_dir == $parent_dir/* ]]; then
+        f_echo "You need to move the installation directory into the folder!"
+        continue
+    fi
+
+    if [ ! -x "$installation_dir/xsetup" ]; then
+        f_echo "xsetup file not found or not executable in the specified directory!"
+        continue
+    fi
+
+    f_echo "Valid Vivado installation directory detected."
+    break
 done
 
-# write file path to "install_bin"
-install_bin_path="${installation_binary#$parent_dir}"
-install_bin_path="/home/user$install_bin_path"
-echo -n "$install_bin_path" > "$script_dir/install_bin"
+# write directory path to "install_dir"
+install_dir_path="${installation_dir#$parent_dir}"
+install_dir_path="/home/user$install_dir_path"
+echo -n "$install_dir_path" > "$script_dir/install_dir"
 
 # Make the user own the whole folder
-if ! chown -R $current_user "$script_dir/.."
-then
-	f_echo "Higher privileges are required to make the folder owned by the user."
-	if ! sudo chown -R $current_user "$script_dir/.."
-	then
-		f_echo "Error setting $current_user as owner of this folder."
-		exit 1
-	fi
+if ! chown -R $current_user "$script_dir/.."; then
+    f_echo "Higher privileges are required to make the folder owned by the user."
+    if ! sudo chown -R $current_user "$script_dir/.."; then
+        f_echo "Error setting $current_user as owner of this folder."
+        exit 1
+    fi
 fi
 
 # Make the scripts executable
-if xattr -p com.apple.quarantine "$script_dir/xvcd/bin/xvcd" &>/dev/null
-then
-	if ! xattr -d com.apple.quarantine "$script_dir/xvcd/bin/xvcd"
-	then
-		f_echo "You need to remove the quarantine attribute from $script_dir/xvcd/bin/xvcd manually."
-		wait_for_user_input
-	fi
+if xattr -p com.apple.quarantine "$script_dir/xvcd/bin/xvcd" &>/dev/null; then
+    if ! xattr -d com.apple.quarantine "$script_dir/xvcd/bin/xvcd"; then
+        f_echo "You need to remove the quarantine attribute from $script_dir/xvcd/bin/xvcd manually."
+        wait_for_user_input
+    fi
 fi
 
-if ! chmod +x "$script_dir"/*.sh "$script_dir/xvcd/bin/xvcd" "$installation_binary"
-then
-	f_echo "Error making the scripts executable."
-	exit 1
+if ! chmod +x "$script_dir"/*.sh "$script_dir/xvcd/bin/xvcd" "$installation_dir/xsetup"; then
+    f_echo "Error making the scripts executable."
+    exit 1
 fi
 
 # make sure that Docker is installed
@@ -127,9 +115,8 @@ start_docker
 eval "$script_dir/configure_docker.sh"
 
 # Generate the Docker image
-if ! eval "$script_dir/gen_image.sh"
-then
-	exit 1
+if ! eval "$script_dir/gen_image.sh"; then
+    exit 1
 fi
 
 # Set VNC resolution
@@ -137,14 +124,13 @@ f_echo "Set the resolution of the container. Keep in mind that high resolutions 
 f_echo "You can change the resolution manually in the vnc_resolution file later."
 f_echo "Press enter to leave the default (1920x1080) or type in your preference:"
 read resolution
-# if resolution has the right format
-if [[ $resolution =~ "^[0-9]+x[0-9]+$" ]]
-then
-	f_echo "Setting $resolution as resolution"
-	echo "$resolution" > "$script_dir/vnc_resolution"
+
+if [[ $resolution =~ "^[0-9]+x[0-9]+$" ]]; then
+    f_echo "Setting $resolution as resolution"
+    echo "$resolution" > "$script_dir/vnc_resolution"
 else
-	f_echo "Setting the default of $vnc_default_resolution"
-	echo "$vnc_default_resolution" > "$script_dir/vnc_resolution"
+    f_echo "Setting the default of $vnc_default_resolution"
+    echo "$vnc_default_resolution" > "$script_dir/vnc_resolution"
 fi
 echo ""
 
@@ -155,4 +141,8 @@ mkdir "$script_dir/../Desktop"
 
 # Start container
 f_echo "Now, the container is started (only terminal, no GUI) and the actual installation process begins."
-docker run --init -it --rm --name vivado_container --mount type=bind,source="$script_dir/..",target="/home/user" -p 127.0.0.1:5901:5901 --platform linux/amd64 x64-linux sudo -H -u user bash /home/user/scripts/install_vivado.sh
+docker run --init -it --rm --name vivado_container \
+    --mount type=bind,source="$script_dir/..",target="/home/user" \
+    -p 127.0.0.1:5901:5901 --platform linux/amd64 x64-linux \
+    sudo -H -u user bash /home/user/scripts/install_vivado.sh
+
